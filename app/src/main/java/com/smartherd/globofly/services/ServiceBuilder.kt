13e923379be3169a789d370a109f870ce5d29af1
Touhid <touhidul.islam.official@gmail.com>
2023-07-25 17:54:25 +0600

    added headerInterceptor to add request headers application-wide.

--- app/src/main/java/com/smartherd/globofly/services/ServiceBuilder.kt
+++ app/src/main/java/com/smartherd/globofly/services/ServiceBuilder.kt
@@ -1,9 +1,13 @@
 package com.smartherd.globofly.services
 
+import android.os.Build
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.Locale
 
 object ServiceBuilder {
     /**
@@ -14,13 +18,30 @@ object ServiceBuilder {
     private var BASE_URL  = "http://10.0.2.2:9000/"
     private val logger = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
 
-    // custom setter for BASE_URL
+    /**
+     * Custom Interceptor
+     * to apply Headers application-wide
+     */
+    private val headerInterceptor = Interceptor { chain ->
+        var request = chain.request()
+        request = request.newBuilder()
+            .addHeader("x-device-type", Build.DEVICE)
+            .addHeader("Accept-Language", Locale.getDefault().language)
+            .build()
+        chain.proceed(request)  //resume the request to the server and return the response
+    }
+
+    /**
+     * custom setter for BASE_URL
+     */
     fun setBaseURL(url: String) {
         BASE_URL = url
     }
 
     // Creating OkHttp Client
-    private val okHTTP = OkHttpClient.Builder().addInterceptor(logger)
+    private val okHTTP = OkHttpClient.Builder()
+        .addInterceptor(headerInterceptor)
+        .addInterceptor(logger)
 
     // Creating Retrofit Builder
     private val builder = Retrofit.Builder()
